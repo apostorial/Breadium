10fddbc63f58500f99c0853bfd26c9abc3003223
apostorial <t.elouadrassi@gmail.com>
2024-10-30 00:35:37 +0100

    New ModToolsMaterials file for custom tools.

---
+++ src/main/java/com/apostorial/item/ModToolMaterials.java
@@ -0,0 +1,57 @@
+package com.apostorial.item;
+
+import com.apostorial.util.ModTags;
+import com.google.common.base.Suppliers;
+import net.minecraft.block.Block;
+import net.minecraft.item.ItemConvertible;
+import net.minecraft.item.ToolMaterial;
+import net.minecraft.recipe.Ingredient;
+import net.minecraft.registry.tag.TagKey;
+
+import java.util.function.Supplier;
+
+public enum ModToolMaterials implements ToolMaterial {
+    BREADIUM(ModTags.Blocks.INCORRECT_FOR_BREADIUM_TOOL, 3000, 12.0F, 5.0F, 15, () -> {
+        return Ingredient.ofItems(new ItemConvertible[]{ModItems.BREADIUM_INGOT});
+    });
+
+    private final TagKey<Block> inverseTag;
+    private final int itemDurability;
+    private final float miningSpeed;
+    private final float attackDamage;
+    private final int enchantability;
+    private final Supplier<Ingredient> repairIngredient;
+
+    private ModToolMaterials(final TagKey<Block> inverseTag, final int itemDurability, final float miningSpeed, final float attackDamage, final int enchantability, final Supplier<Ingredient> repairIngredient) {
+        this.inverseTag = inverseTag;
+        this.itemDurability = itemDurability;
+        this.miningSpeed = miningSpeed;
+        this.attackDamage = attackDamage;
+        this.enchantability = enchantability;
+        this.repairIngredient = Suppliers.memoize(repairIngredient::get);
+    }
+
+    public int getDurability() {
+        return this.itemDurability;
+    }
+
+    public float getMiningSpeedMultiplier() {
+        return this.miningSpeed;
+    }
+
+    public float getAttackDamage() {
+        return this.attackDamage;
+    }
+
+    public TagKey<Block> getInverseTag() {
+        return this.inverseTag;
+    }
+
+    public int getEnchantability() {
+        return this.enchantability;
+    }
+
+    public Ingredient getRepairIngredient() {
+        return this.repairIngredient.get();
+    }
+}
