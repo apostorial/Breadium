c06da3ced4348cf34693189e2be663ccb76cebc4
apostorial <t.elouadrassi@gmail.com>
2024-10-29 12:57:39 +0100

    Initialized the Breadium Star as a fuel source.

--- src/main/java/com/example/Breadium.java
+++ src/main/java/com/example/Breadium.java
@@ -5,6 +5,7 @@ import com.example.item.ModItemGroups;
 import com.example.item.ModItems;
 import net.fabricmc.api.ModInitializer;
 
+import net.fabricmc.fabric.api.registry.FuelRegistry;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -17,6 +18,7 @@ public class Breadium implements ModInitializer {
 		ModItems.initialize();
 		ModItemGroups.initialize();
 		ModBlocks.initialize();
+		FuelRegistry.INSTANCE.add(ModItems.BREADIUM_STAR, 631138504); // 631,138,504.32 ticks exactly, 31,556,925.216 seconds, 365.24219 days, 1 solar year
 		LOGGER.info("Hello Fabric world!");
 	}
 }
