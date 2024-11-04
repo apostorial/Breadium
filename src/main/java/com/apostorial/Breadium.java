c1bc2370be059016202d68bb4ef17afc01b99724
Apostorial <t.elouadrassi@gmail.com>
2024-11-04 12:12:48 +0100

    Added natural generation for the breadium ore..

--- src/main/java/com/apostorial/Breadium.java
+++ src/main/java/com/apostorial/Breadium.java
@@ -5,7 +5,17 @@ import com.apostorial.item.ModItemGroups;
 import com.apostorial.item.ModItems;
 import net.fabricmc.api.ModInitializer;
 
+import net.fabricmc.fabric.api.biome.v1.BiomeModifications;
+import net.fabricmc.fabric.api.biome.v1.BiomeSelectors;
 import net.fabricmc.fabric.api.registry.FuelRegistry;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.registry.RegistryKey;
+import net.minecraft.registry.RegistryKeys;
+import net.minecraft.util.Identifier;
+import net.minecraft.world.gen.GenerationStep;
+import net.minecraft.world.gen.feature.OreFeature;
+import net.minecraft.world.gen.feature.OreFeatureConfig;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -19,6 +29,16 @@ public class Breadium implements ModInitializer {
 		ModItemGroups.initialize();
 		ModBlocks.initialize();
 		FuelRegistry.INSTANCE.add(ModItems.BREADIUM_STAR, Integer.MAX_VALUE);
+		Registry.register(Registries.FEATURE,
+				Identifier.of(MOD_ID, "breadium_ore"),
+				new OreFeature(OreFeatureConfig.CODEC));
+
+		BiomeModifications.addFeature(
+				BiomeSelectors.foundInOverworld(),
+				GenerationStep.Feature.UNDERGROUND_ORES,
+				RegistryKey.of(RegistryKeys.PLACED_FEATURE,
+						Identifier.of(MOD_ID, "breadium_ore"))
+		);
 		LOGGER.info("Hello Fabric world!");
 	}
 }
